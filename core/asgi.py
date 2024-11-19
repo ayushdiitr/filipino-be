edd256c06244475966e3cbd3a7809ce8e7643c12
Ayush Dhiman <ayushd175@gmail.com>
2024-11-19 23:50:08 +0530

    routing changes

--- core/asgi.py
+++ core/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 
@@ -17,9 +18,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(   
         URLRouter(
             websocket_urlpatterns
-        )
+        ))
     ),
 })
