17697d44ae79724ddeaa94b70176d2336b21d110
Ayush Dhiman <ayushd175@gmail.com>
2024-12-14 14:14:01 +0530

    get profile details api

--- userProfile/urls.py
+++ userProfile/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import swipe_user
+from .views import swipe_user, get_profile_data
 
 urlpatterns = [
-    path('swipe/<str:user_id>/<str:action>/', swipe_user, name='swipe_user')
+    path('swipe/<str:user_id>/<str:action>/', swipe_user, name='swipe_user'),
+    path('profileDetails/<str:user_id>/', get_profile_data, name='get_profile_details')
 ]
--- userProfile/views.py
+++ userProfile/views.py
@@ -2,7 +2,24 @@ from django.http import JsonResponse
 from models.user import User
 from models.userProfile import Profile
 from bson import ObjectId
+from django.views.decorators.csrf import csrf_exempt
+import logging
+from user.views import user_to_dict
 
+logger = logging.getLogger(__name__)
+
+
+
+def profile_to_dict(profile, liked_users):
+    return {
+        "user": user_to_dict(profile.user),
+        "liked_users": [user_to_dict(user) for user in liked_users],
+        "work": profile.work,
+        "education": profile.education,
+        "location": profile.location,
+        "looking_for": profile.looking_for,
+        "interests": profile.interests
+    }
 
 # Create your views here.
 def swipe_user(request, user_id, action):
@@ -42,4 +59,31 @@ def swipe_user(request, user_id, action):
     except Profile.DoesNotExist:
         return JsonResponse({'error': 'Profile not found'}, status=404)
     except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+
+@csrf_exempt
+def get_profile_data(request, user_id):
+    try:
+        logger.info("Starting get_profile_data function")
+
+        user = User.objects.get(user_id=user_id)
+
+        profile = Profile.objects.get(user=user)
+        liked_users = User.objects.filter(user_id__in=[liked_user.user_id for liked_user in profile.liked_users])
+
+        profile_data = profile_to_dict(profile, liked_users)
+
+        return JsonResponse({'message':'Profile fetched', 'data':profile_data}, status=200, safe=False)
+
+    except User.DoesNotExist: 
+        logger.error("User with user_id %s does not exist", user_id)
+        return JsonResponse({'error': 'User not found'}, status=404)
+
+    except Profile.DoesNotExist:
+        logger.error("Profile for user with user_id %s does not exist", user_id)
+        return JsonResponse({'error': 'Profile not found'}, status=404)
+
+    except Exception as e:
+        logger.error("An error occurred: %s", str(e))
         return JsonResponse({'error': str(e)}, status=500)
